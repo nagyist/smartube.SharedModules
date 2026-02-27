5e25131daa081245c3651ef03d3ff1ce392ab97a
Yuriy Liskov <firsthash@gmail.com>
2026-02-27 14:02:10 +0200

    const upd; cronet: add quic experimental options

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/cronet/CronetManager.kt
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/cronet/CronetManager.kt
@@ -3,6 +3,7 @@ package com.liskovsoft.sharedutils.cronet
 import android.content.Context
 import com.liskovsoft.sharedutils.mylogger.Log
 import org.chromium.net.CronetEngine
+import org.chromium.net.ExperimentalCronetEngine
 import org.chromium.net.impl.NativeCronetProvider
 
 object CronetManager {
@@ -22,13 +23,43 @@ object CronetManager {
             //    .build()
 
             try {
-                engine = NativeCronetProvider(context)
-                    .createBuilder()
+                val builder = NativeCronetProvider(context).createBuilder()
+
+                builder
                     .enableQuic(true)
                     .enableHttp2(true)
                     .enableBrotli(true)
                     //.addQuicHint("youtube.com", 80, 80)
-                    .build()
+
+                if (builder is ExperimentalCronetEngine.Builder) {
+                    val experimentalOptions = """
+                        {
+                            "AsyncResolver": {
+                                "enable_async_resolver": true
+                            },
+                            "StaleDNS": {
+                                "enable": true,
+                                "max_expired_time_ms": 86400000,
+                                "max_stale_uses": 5,
+                                "persist_to_disk": false,
+                                "use_stale_on_name_not_resolved": true
+                            },
+                            "QUIC": {
+                                "delay_tcp_race": true,
+                                "estimate_initial_rtt": true,
+                                "max_server_configs_stored_in_properties": 8,
+                                "idle_connection_timeout_seconds": 30,
+                                "goaway_sessions_on_ip_change": true,
+                                "initial_rtt_for_handshake_milliseconds": 100,
+                                "allow_other_network_reuse": true
+                            }
+                        }
+                    """.trimIndent()
+
+                    builder.setExperimentalOptions(experimentalOptions)
+                }
+
+                engine = builder.build()
             } catch (e: UnsatisfiedLinkError) {
                 // Fatal Exception: java.lang.UnsatisfiedLinkError
                 // Cannot load library: soinfo_relocate(linker.cpp:982): cannot locate symbol "getauxval" referenced by "libcronet.101.0.4951.41.so".
